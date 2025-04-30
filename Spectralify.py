c566d76b65ac0fafcbd3abea56992a9729c479e1
Luke Cutter <50847121+Luke-Cutter@users.noreply.github.com>
2025-04-29 23:33:28 -0400

    Spectalify Refactor
    Full refactor of Spectralify with no feature left behind!

--- Spectralify.py
+++ Spectralify.py
@@ -1155,7 +1155,7 @@ def main():
         music_directory = sys.argv[1]
     else:
         # Default directory - change this to your music directory
-        music_directory = "C:\\Users\\cutte\\Music\\Spoon\\Girls Can't Tell (EVIL)"
+        music_directory = "C://Users//Change//This//Path"
     
     # Run the application
     app = SpectralifyApp(music_directory)
